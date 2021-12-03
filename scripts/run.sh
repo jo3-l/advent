7a227dc4ff2bda86517f28b17b0ae72f08e914f8
Joe <56809242+Jo3-L@users.noreply.github.com>
2021-12-02 20:02:20 -0800

    refactor(scripts): change run output

--- scripts/run.sh
+++ scripts/run.sh
@@ -6,8 +6,8 @@ script_dir=$(dirname $full_path)
 project_dir=$(dirname $script_dir)
 day_dir=$project_dir/$YEAR/$padded_day
 
-if [[ $1 == "-s" ]]; then
-	py ${day_dir}/solution.py ${day_dir}/sample.txt
-else
-	py ${day_dir}/solution.py ${day_dir}/input.txt
-fi
+echo "SAMPLE OUTPUT"
+py ${day_dir}/solution.py ${day_dir}/sample.txt
+echo "---"
+echo "OUTPUT"
+py ${day_dir}/solution.py ${day_dir}/input.txt
