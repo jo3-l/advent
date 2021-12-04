a32a61cd3ebd956e78acd1dfe0dd7fd87f4b2f66
Joe <56809242+Jo3-L@users.noreply.github.com>
2021-12-03 17:43:55 -0800

    chore(*): update tooling

--- template.py
+++ template.py
@@ -3,7 +3,11 @@ from functools import cache
 import math
 import re
 import itertools
-import sys
+import os
+from heapq import heappush, heappop
+
+adj4 = ((0, -1), (0, 1), (1, 0), (-1, 0))
+adj8 = ((1, 0), (-1, 0), (0, 1), (0, -1), (1, -1), (1, 1), (-1, 1), (-1, -1))
 
 
 def lmap(f, it):
@@ -14,12 +18,31 @@ def ints(txt):
     return lmap(int, txt)
 
 
-with open(sys.argv[1]) as f:
-    input = f.read()
+def make_indexer(lst, default=None):
+    def get(*indices):
+        cur = lst
+        for i in indices:
+            if 0 <= i < len(cur):
+                cur = cur[i]
+            else:
+                return default
+        return cur
+
+    return get
 
 
 def solve(input):
     ...
 
 
-print(solve(input.rstrip("\n")))
+cur_dir = os.path.dirname(os.path.realpath(__file__))
+
+print("SAMPLE OUTPUT")
+with open(os.path.join(cur_dir, "sample.txt")) as f:
+    print(solve(f.read()))
+
+print("---")
+
+print("OUTPUT")
+with open(os.path.join(cur_dir, "input.txt")) as f:
+    print(solve(f.read()))
