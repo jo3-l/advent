3ecb956dc700f3dd5426060f5a0e56f067dd13a7
Joe <56809242+Jo3-L@users.noreply.github.com>
2021-12-03 19:23:26 -0800

    feat(scripts): add watch script

---
+++ scripts/watch.sh
@@ -0,0 +1,5 @@
+source ./config.txt
+
+padded_day=$(printf %02s $DAY)
+project_dir=$(realpath $0 | xargs dirname | xargs dirname)
+nodemon --config "$project_dir/nodemon.json" "$project_dir/$YEAR/$padded_day/solution.py"
