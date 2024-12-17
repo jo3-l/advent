baa990908787e366d611db3937b04316c05571bc
Joseph Liu <jo3.l.dev@outlook.com>
2024-12-17 02:28:54 -0500

    16: clean up slightly

--- 2024/16/solution.py
+++ 2024/16/solution.py
@@ -1,18 +1,18 @@
 from dataclasses import dataclass
-from heapq import heappop
+from heapq import heappop, heappush
 from functools import total_ordering
 from typing import Optional
-import math
+from collections import defaultdict, deque
 
 # in clockwise order, starting with east
 dirs = ((0, 1), (1, 0), (0, -1), (-1, 0))
 
-StateKey = tuple[int, int, int]
+INF = 10**18
 
 
 @total_ordering
 @dataclass
-class Work:
+class Item:
     i: int
     j: int
     dir: int
@@ -21,7 +21,7 @@ class Work:
     def state(self) -> "State":
         return State(self.i, self.j, self.dir)
 
-    def __lt__(self, other: "Work"):
+    def __lt__(self, other: "Item"):
         return (self.cost, self.dir, self.i, self.j) < (
             other.cost,
             other.dir,
@@ -38,89 +38,105 @@ class State:
 
 
 @dataclass
-class OptPathInfo:
+class OptimalPathInfo:
     cost: int
-    predecessors: set[State]
+    parents: set[State]
+
+
+class WorkState:
+    def __init__(self, maze: list[list[str]]):
+        self.q = []
+        self.best = defaultdict(lambda: OptimalPathInfo(INF, set()))
+        self.maze = maze
+
+    def add(self, item: Item, parent: Optional[State] = None):
+        state = item.state()
+        opt = self.best[state]
+        if item.cost == opt.cost:
+            if parent:
+                if parent not in opt.parents:
+                    opt.parents.add(parent)
+                    self._push(item)
+        elif item.cost < opt.cost:
+            opt.cost = item.cost
+            opt.parents.clear()
+            if parent:
+                opt.parents.add(parent)
+            self._push(item)
+
+    def _push(self, item: Item):
+        if self.maze[item.i][item.j] != "E":
+            heappush(self.q, item)
+
+    def done(self) -> bool:
+        return not self.q
+
+    def pop(self) -> Item:
+        return heappop(self.q)
+
+    def min_cost_to(self, i: int, j: int) -> Optional[tuple[int, list[State]]]:
+        min_cost = INF
+        min_states = []
+        for d in range(4):
+            state = State(i, j, d)
+            cost = self.best[state].cost
+            if cost != INF:
+                if cost == min_cost:
+                    min_states.append(state)
+                elif cost < min_cost:
+                    min_cost = cost
+                    min_states = [state]
+
+        if min_cost == INF:
+            return None
+        else:
+            return min_cost, min_states
+
+    def backtrack(self, end_states: list[State]) -> set[State]:
+        seen = set(end_states)
+        q = deque(end_states)
+        while q:
+            state = q.popleft()
+            for parent in self.best[state].parents:
+                if parent not in seen:
+                    seen.add(parent)
+                    q.append(parent)
+        return seen
 
 
 def solve(data: str):
     maze = list(map(list, data.splitlines()))
 
-    best: dict[State, OptPathInfo] = {}
-    q: list[Work] = []
-
-    def push(w: "Work"):
-        if maze[w.i][w.j] != "E":
-            q.append(w)
-
-    def process(w: "Work", prev: Optional[State] = None):
-        k = w.state()
-        try:
-            opt = best[k]
-            if w.cost == opt.cost:
-                if prev:
-                    if prev not in opt.predecessors:
-                        opt.predecessors.add(prev)
-                        push(w)
-                else:
-                    push(w)
-            elif w.cost < opt.cost:
-                best[k] = OptPathInfo(w.cost, predecessors=set([prev] if prev else []))
-                push(w)
-        except KeyError:
-            best[k] = OptPathInfo(w.cost, predecessors=set([prev] if prev else []))
-            push(w)
-
+    work = WorkState(maze)
     end_i, end_j = -1, -1
     for i, row in enumerate(maze):
         for j, c in enumerate(row):
             if c == "S":
-                process(Work(i, j, dir=0, cost=0))
+                work.add(Item(i, j, dir=0, cost=0))
             elif c == "E":
                 end_i, end_j = i, j
 
-    while q:
-        w = heappop(q)
-        if w.cost > best[w.state()].cost:
+    while not work.done():
+        item = work.pop()
+        state = item.state()
+        if item.cost > work.best[state].cost:
             continue
 
-        for ndir in ((w.dir - 1) % 4, (w.dir + 1) % 4):
-            process(Work(w.i, w.j, dir=ndir, cost=w.cost + 1000), prev=w.state())
+        for ndir in ((item.dir - 1) % 4, (item.dir + 1) % 4):
+            work.add(
+                Item(item.i, item.j, dir=ndir, cost=item.cost + 1000),
+                parent=state,
+            )
 
-        dy, dx = dirs[w.dir]
-        ni, nj = w.i + dy, w.j + dx
+        dy, dx = dirs[item.dir]
+        ni, nj = item.i + dy, item.j + dx
         if maze[ni][nj] != "#":
-            process(Work(ni, nj, dir=w.dir, cost=w.cost + 1), prev=w.state())
+            work.add(Item(ni, nj, dir=item.dir, cost=item.cost + 1), parent=state)
 
-    min_cost = math.inf
-    min_cost_states = []
-    for dir in range(4):
-        k = State(end_i, end_j, dir)
-        try:
-            opt = best[k]
-            if opt.cost == min_cost:
-                min_cost_states.append(k)
-            elif opt.cost < min_cost:
-                min_cost = opt.cost
-                min_cost_states = [k]
-        except KeyError:
-            pass
+    best = work.min_cost_to(end_i, end_j)
+    assert best, "no path found"
 
+    min_cost, end_states = best
     print(f"min cost: {min_cost}")
-    good_tiles: set[tuple[int, int]] = set([(end_i, end_j)])
-
-    visited_states = set()
-
-    def visit(k: State):
-        if k in visited_states:
-            return
-        visited_states.add(k)
-        opt = best[k]
-        for p in opt.predecessors:
-            good_tiles.add((p.i, p.j))
-            visit(p)
-
-    for k in min_cost_states:
-        visit(k)
-
+    good_tiles = set((state.i, state.j) for state in work.backtrack(end_states))
     print(f"good tiles: {len(good_tiles)}")
