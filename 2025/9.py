527430624af2bf7e2e625f26a67ea296a6dc1b34
Joseph Liu <jo3.l.dev@outlook.com>
2025-12-26 10:35:07 -0800

    day9: more correct

--- 2025/9.py
+++ 2025/9.py
@@ -3,7 +3,7 @@ import bisect
 import sys
 from itertools import pairwise, chain
 
-sys.setrecursionlimit(10**9)
+sys.setrecursionlimit(10**9)  # for `floodfill`
 
 Point = namedtuple("Point", ["y", "x"])
 
@@ -29,44 +29,55 @@ def solve1(s: str):
 class RectSumQueries:
     def __init__(self, A):
         R, C = len(A), len(A[0])
-        S = [[0] * (C + 1) for _ in range(R + 1)]  # 1-indexed
+        topleft_sum = [[0] * (C + 1) for _ in range(R + 1)]  # 1-indexed
         for r in range(1, R + 1):
             for c in range(1, C + 1):
-                S[r][c] = A[r - 1][c - 1] + S[r][c - 1] + S[r - 1][c] - S[r - 1][c - 1]
-        self.S = S
-        self.A = A  # TODO
+                topleft_sum[r][c] = (
+                    A[r - 1][c - 1]
+                    + topleft_sum[r][c - 1]
+                    + topleft_sum[r - 1][c]
+                    - topleft_sum[r - 1][c - 1]
+                )
+        self.topleft_sum = topleft_sum
 
     def sum(self, tl: Point, br: Point):
         tl_r, tl_c = tl.y + 1, tl.x + 1  # convert to 1-indexed
         br_r, br_c = br.y + 1, br.x + 1
-
-        S = self.S
         return (
-            S[br_r][br_c]
-            - S[tl_r - 1][br_c]
-            - S[br_r][tl_c - 1]
-            + S[tl_r - 1][tl_c - 1]
+            self.topleft_sum[br_r][br_c]
+            - self.topleft_sum[tl_r - 1][br_c]
+            - self.topleft_sum[br_r][tl_c - 1]
+            + self.topleft_sum[tl_r - 1][tl_c - 1]
         )
+
+
+def compact(xs):
+    N = len(xs)
+    i, j = 0, 0
+    while i < N:
+        xs[j] = xs[i]
+        j += 1
+
+        start = i
+        i += 1
+        while i < N and xs[i] == xs[start]:
+            i += 1
+    del xs[j:]
+    return xs
 
 
 class CoordCompress:
     def __init__(self, coords):
-        self.sorted_coords = sorted(set(coords))
+        self.sorted_coords = compact(sorted(coords))
 
     def map(self, old):
-        idx = bisect.bisect(self.sorted_coords, old) - 1
-        assert self.sorted_coords[idx] == old
-        return idx
+        i = bisect.bisect_left(self.sorted_coords, old)
+        assert self.sorted_coords[i] == old
+        return i
 
     @property
-    def max_mapped(self):
+    def max(self):
         return len(self.sorted_coords) - 1
-
-
-def between(lo, hi):
-    if lo > hi:
-        lo, hi = hi, lo
-    return range(lo, hi + 1)
 
 
 def neighbors(r, c):
@@ -77,28 +88,26 @@ def neighbors(r, c):
             yield (r + dr, c + dc)
 
 
-# returns whether the floodfill hit the boundary of A
-def floodfillnegative(A, r, c):
+def floodfill(A, r, c, oldcolor, newcolor):
+    """Returns whether the floodfill hit the boundary of `A`."""
     R, C = len(A), len(A[0])
-    assert A[r][c] == 0
-    A[r][c] = -1
+    assert A[r][c] == oldcolor
+
+    A[r][c] = newcolor
+    hit_boundary = False
     for nr, nc in neighbors(r, c):
         if 0 <= nr < R and 0 <= nc < C:
-            if A[nr][nc] == 0:
-                outside = floodfillnegative(A, nr, nc)
-                if outside:
-                    return True
+            if A[nr][nc] == oldcolor:
+                hit_boundary |= floodfill(A, nr, nc, oldcolor, newcolor)
         else:
-            return True
-    return False
+            hit_boundary = True
+    return hit_boundary
 
 
-def repaint(A, oldcolor, newcolor):
-    R, C = len(A), len(A[0])
-    for r in range(R):
-        for c in range(C):
-            if A[r][c] == oldcolor:
-                A[r][c] = newcolor
+def between(lo, hi):
+    if lo > hi:
+        lo, hi = hi, lo
+    return range(lo, hi + 1)
 
 
 def solve2(s: str):
@@ -107,16 +116,18 @@ def solve2(s: str):
         x, y = map(int, line.split(","))
         points.append(Point(y, x))
 
-    Y = CoordCompress((pt.y for pt in points))
-    X = CoordCompress((pt.x for pt in points))
+    y_compress = CoordCompress((pt.y for pt in points))
+    x_compress = CoordCompress((pt.x for pt in points))
 
-    def map_pt(p: Point):
-        return Point(Y.map(p.y), X.map(p.x))
+    def compress(p: Point):
+        return Point(y_compress.map(p.y), x_compress.map(p.x))
 
-    A = [[0] * (X.max_mapped + 1) for _ in range(Y.max_mapped + 1)]
+    R = y_compress.max + 1
+    C = x_compress.max + 1
+    A = [[0] * C for _ in range(R)]
     for a, b in pairwise(chain(points, [points[0]])):
-        ya, xa = map_pt(a)
-        yb, xb = map_pt(b)
+        ya, xa = compress(a)
+        yb, xb = compress(b)
         if ya == yb:
             y = ya
             for x in between(xa, xb):
@@ -127,24 +138,31 @@ def solve2(s: str):
             for y in between(ya, yb):
                 A[y][x] = 1
 
-    # color the interior of the region +1
-    for r, c in neighbors(*map_pt(points[0])):
-        if 0 <= r < len(A) and 0 <= c < len(A[r]) and A[r][c] == 0:
-            outside = floodfillnegative(A, r, c)
-            if outside:
-                repaint(A, -1, 0)  # retry
-            else:
-                repaint(A, -1, +1)  # done
-                break
+    # Color the interior of the region +1.
+    interior_colors = {1}
+    next_color = 2
+    for r in range(R):
+        for c in range(C):
+            if A[r][c] == 0:
+                color = next_color
+                exterior = floodfill(A, r, c, oldcolor=0, newcolor=color)
+                if not exterior:
+                    interior_colors.add(color)
+                next_color += 1
+
+    for r in range(R):
+        for c in range(C):
+            A[r][c] = A[r][c] in interior_colors
 
     rsq = RectSumQueries(A)
     N = len(points)
     best = 0
     for i in range(N):
         for j in range(i + 1, N):
-            u, v = map_pt(points[i]), map_pt(points[j])
-            tl = Point(min(u.y, v.y), min(u.x, v.x))
-            br = Point(max(u.y, v.y), max(u.x, v.x))
+            ya, xa = compress(points[i])
+            yb, xb = compress(points[j])
+            tl = Point(min(ya, yb), min(xa, xb))
+            br = Point(max(ya, yb), max(xa, xb))
             if rsq.sum(tl, br) == area(tl, br):
                 best = max(best, area(points[i], points[j]))
     return best
